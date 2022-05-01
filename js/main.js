e237c5a0aa578fc1d09076adc566201d0d88a644
bhuvan-dv <bh123social@gmail.com>
2022-05-01 09:56:27 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+// let tshirt = document.getElementById("tshirt");
+// let sunglass = document.getElementById("sunglass");
+// // tshirt.style.display = "block";
+// // sunglass.style.display = "none";
+// let tshirtAnchor = document.getElementById("tshirtAnchor");
+// let sunglassAnchor = document.getElementById("sunglassAnchor");
+// tshirtAnchor.addEventListener("click", (e) => {
+//    let active=tshirt.classList.toggle("active")
+//     if (active === true) tshirt.style.display = "block";
+//     else tshirt.style.display = "none";
+
+// })
+// sunglassAnchor.addEventListener("click", (e) => {
+//      let active = sunglass.classList.toggle("active");
+//      if (active === true) sunglass.style.display = "block";
+//      else sunglass.style.display = "none";
+// })
